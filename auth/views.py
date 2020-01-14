2b62a76726ff6cce3002bdf04d534dcd78b2527c
munene19 <munenexv@gmail.com>
2020-01-14 15:39:51 +0300

    Add registration

--- auth/views.py
+++ auth/views.py
@@ -24,3 +24,14 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))

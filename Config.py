4f269d2dbf074019134fa3554284b9f329968dfd
munene19 <munenexv@gmail.com>
2020-01-14 21:12:54 +0300

    Layout re-created

--- Config.py
+++ Config.py
@@ -1,21 +1,21 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-
-class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:Backrow@localhost/watchlist'
-
-
-class DevConfig(Config):
-    DEBUG = True
-
-
-config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
-}
+# import os
+#
+#
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+#
+#
+# class ProdConfig(Config):
+#     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:Backrow@localhost/watchlist'
+#
+#
+# class DevConfig(Config):
+#     DEBUG = True
+#
+#
+# config_options = {
+#     'development': DevConfig,
+#     'production': ProdConfig
+# }
---
+++ app/pitch.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, template_folder='templates')
+
+posts = [
+    {
+        'author': 'Thee Mike',
+        'title': ' Pitch 1',
+        'content': 'First pitch content',
+        'date_posted': 'May 6,2020'
+    },
+    {
+        'author': 'Mary Jane',
+        'title': 'pitch 2',
+        'content': 'Second pitch content',
+        'date_posted': 'May 6, 2020'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home.html")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

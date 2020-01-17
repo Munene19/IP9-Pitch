e6e2d87f925694bf831169688aa304aa53502a63
munene19 <munenexv@gmail.com>
2020-01-17 10:12:38 +0300

    Heroku deployment

--- Config.py
+++ Config.py
@@ -21,3 +21,60 @@ config_options = {
     'development': DevConfig,
     'production': ProdConfig
 }
+# class Config:
+
+#     '''
+#     Parent configuration class
+#     '''
+
+#     debug = True
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:1234@localhost/pitch'
+
+#     #  email configurations
+
+#     MAIL_SERVER = 'smtp.gmail.com'
+#     MAIL_PORT = 465
+#     MAIL_USE_TLS = False
+#     MAIL_USE_SSL = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+# class TestConfig(Config):
+#     '''
+#     Testing configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:1234@localhost/pitch'
+
+
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+
+
+#     SECRET_KEY = 'hahaha'
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:hahaa@localhost/pitch'
+#     DEBUG = True
+#     ENV = 'development'
+
+
+# config_options = {
+#     'development':DevConfig,
+#     'production':ProdConfig,
+#     'test':TestConfig
+# }
